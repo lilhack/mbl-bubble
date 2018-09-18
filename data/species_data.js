49fd11ca7b9a4ff927759ecb0ade1bef35d2ea07
Lilly Hackworth <lilly@hackworth.org>
2018-09-18 15:41:11 -0400

    More data options

---
+++ data/species_data.js
@@ -0,0 +1,152 @@
+var species = {
+  snailfur: {
+    center: {lat: 41.513921, lng: -70.677},
+    population: 1,
+    color: '#ffc0cb'
+  },
+  long_claw_hermit: {
+    center: {lat: 41.51372, lng: -70.677252},
+    population: 20,
+    color: '#8a2be2'
+  },
+  plumed_worm: {
+    center: {lat: 41.51372, lng: -70.677252},
+    population: 1,
+    color: '#e65b00'
+  },
+  bivalve: {
+    center: {lat: 41.510323, lng: -70.706405},
+    population: 1,
+    color: '#00b32d'
+  },
+  tiny_clam: {
+    center: {lat: 41.513833, lng: -70.676936},
+    population: 1,
+    color: '#006700'
+  },
+  tiny_tube_worm: {
+    center: {lat: 41.512795, lng: -70.703365},
+    population: 15,
+    color: '#ffa500'
+  },
+  sandworm: {
+    center: {lat: 41.513845, lng: -70.677016},
+    population: 30,
+    color: '#b37400'
+  },
+  jointworm: {
+    center: {lat: 41.513889, lng: -70.676965},
+    population: 3,
+    color: '#ffc04d'
+  },
+  green_crab: {
+    center: {lat: 41.512827, lng: -70.703304},
+    population: 1,
+    color: '#800080'
+  },
+  sea_cucumber: {
+    center: {lat: 41.513833, lng: -70.677132},
+    population: 10,
+    color: '#008080'
+  },
+  mud_snail:{
+    center: {lat: 41.512755, lng: -70.703357},
+    population: 1,
+    color: '#ffff00'
+  },
+  tangle_worm: {
+    center: {lat: 41.513879, lng: -70.6769},
+    population: 10,
+    color: '#ff6500'
+  },
+  sea_cucumber2: {
+    center: {lat: 41.514234, lng: -70.676346},
+    population: 5,
+    color: '#008080'
+  },
+  mud_snail2: {
+    center: {lat: 41.575943, lng: -70.64145},
+    population: 1,
+    color: '#ffff00'
+  },
+  flat_claw: {
+    center: {lat: 41.576656, lng: -70.639003},
+    population: 5,
+    color: '#6c2be2'
+  },
+  long_claw: {
+    center: {lat: 41.576672, lng: -70.639038},
+    population: 20,
+    color: '#8a2be2'
+  },
+  sponge: {
+    center: {lat: 41.576739, lng: -70.638938},
+    population: 1,
+    color: '#ff0000'
+  },
+  spider_small: {
+    center: {lat: 41.576305, lng: -70.640032},
+    population: 1,
+    color: '#0015ff'
+  },
+  hard_clam: {
+    center: {lat: 41.57677, lng: -70.642},
+    population: 20,
+    color: '#00ff00'
+  },
+  big_spider: {
+    center: {lat: 41.577084, lng: -70.640454},
+    population: 1,
+    color: '#0015ff'
+  },
+  mussel: {
+    center: {lat: 41.516512, lng: -70.657752},
+    population: 5,
+    color: '#00b300'
+  },
+  eel: {
+    center: {lat:  41.576491, lng: -70.639459},
+    population: 2,
+    color: '#1dffb3'
+  },
+  moon_snail: {
+    center: {lat: 41.576314, lng: -70.637657},
+    population: 1,
+    color: '#ffd700'
+  },
+  longnose_spider: {
+    center: {lat: 41.516602, lng: -70.657694},
+    population: 1,
+    color: '#2a00ff'
+  },
+  oyster_drill: {
+    center: {lat: 41.51685, lng: -70.657928},
+    population: 1,
+    color: '#e7ff00'
+  },
+  crabs_unknown: {
+    center: {lat: 41.516673, lng: -70.657787},
+    population: 1,
+    color: '#800040'
+  },
+  green_crab2: {
+    center: {lat: 41.516629, lng: -70.65778},
+    population: 3,
+    color: '#cd00cd'
+  },
+  polychaete: {
+    center: {lat: 41.516622, lng: -70.657902},
+    population: 20,
+    color: '#ff7b00'
+  },
+  glycera: {
+    center: {lat: 41.516629, lng: -70.657855},
+    population: 10,
+    color: '#ffd000'
+  },
+  sand_crab: {
+    center: {lat: 41.516613, lng: -70.657845},
+    population: 1,
+    color: '#800060'
+  }
+};
